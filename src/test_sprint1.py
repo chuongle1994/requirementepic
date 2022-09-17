e8cb56df5b8c07a34d91d218662baf5b17b58d3a
LeTri01 <trile0125@gmail.com>
2022-09-17 14:54:09 -0400

    New test

--- src/test_sprint1.py
+++ src/test_sprint1.py
@@ -1,27 +1,46 @@
 
-from functions.loginfunctions import *
+from fileinput import close
+from functions import loginfunctions, createAccountFunctions
 
 
-# Selecting any of these skills result in “under construction”
-def test_frontendDevelopment():
-    assert frontendDevelopment() == "\nunder construction"
+# test if number of account is greater than 5
+def test_accNumTest():
+    createAccountFunctions.storeData("trile", "Abcdef1!")
+    createAccountFunctions.storeData("danhle", "Abcdef1!")
+    createAccountFunctions.storeData("dinhle", "Abcdef1!")
+    createAccountFunctions.storeData("chuongle", "Abcdef1!")
+    createAccountFunctions.storeData("hyunjung", "Abcdef1!")
+    assert createAccountFunctions.checkAccNum() == 1
+    user = open("users.txt", "w")
+    user.close()
+    passw = open("passwords.txt", "w")
+    passw.close()
 
-def test_backendDevelopment():
-    assert backendDevelopment() == "\nunder construction"
+    createAccountFunctions.storeData("trile", "Abcdef1!")
+    createAccountFunctions.storeData("danhle", "Abcdef1!")
+    createAccountFunctions.storeData("dinhle", "Abcdef1!")
+    createAccountFunctions.storeData("chuongle", "Abcdef1!")
+    assert createAccountFunctions.checkAccNum() == 0
+    user = open("users.txt", "w")
+    user.close()
+    passw = open("passwords.txt", "w")
+    passw.close()
 
-def test_databaseDesign():
-    assert databaseDesign() == "\nunder construction"
+# check if there is duplicated account
+def test_checkDup():
+    createAccountFunctions.storeData("trile", "Abcdef1!")
+    assert createAccountFunctions.checkUser("trile") == 1
+    assert createAccountFunctions.checkUser("triiile") == 0
+    user = open("users.txt", "w")
+    user.close()
+    passw = open("passwords.txt", "w")
+    passw.close()
 
-def test_agileMethodologies():
-    assert agileMethodologies() == "\nunder construction"
-
-def test_gitVersionControl():
-    assert gitVersionControl() == "\nunder construction"
-
-# search for job / internship and find someone you know result in "under construction"
-
-def test_searchForJob():
-    assert searchForJob() == "\nunder construction"
-
-def test_findSomeone():
-    assert findSomeone() == "\nunder construction"
+# check if passwords are eligible
+def test_checkPass():
+    assert createAccountFunctions.checkPass("abc") == 1
+    assert createAccountFunctions.checkPass("aaaaaaaaaaa") == 1
+    assert createAccountFunctions.checkPass("AAAAAAAAAAA") == 1
+    assert createAccountFunctions.checkPass("AAAAA!!!!!!") == 1
+    assert createAccountFunctions.checkPass("AAAAA11111") == 1
+    assert createAccountFunctions.checkPass("AAAAAA1111!") == 0
