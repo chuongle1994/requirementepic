41a1c9bc2b0b80323895a321ad3ce4b9eeb40b35
VanDinhLe <77133778+VanDinhLe@users.noreply.github.com>
2022-10-11 13:23:20 -0400

    Add files via upload

--- src/test_name.py
+++ src/test_name.py
@@ -7,17 +7,14 @@ users = [["trile", "Abcdwd1!", "tri", "le", "TriLe"],
     ["hyunjunglee", "Abcdwd1!", "hyunjung", "lee", "HyunjungLee"]]
 
 
-createAccountFunctions.storeData(users[0][0],users[0][1],users[0][2],users[0][3],users[0][4])
-createAccountFunctions.storeData(users[1][0],users[1][1],users[1][2],users[1][3],users[1][4])
-createAccountFunctions.storeData(users[2][0],users[2][1],users[2][2],users[2][3],users[2][4])
-createAccountFunctions.storeData(users[3][0],users[3][1],users[3][2],users[3][3],users[3][4])
-createAccountFunctions.storeData(users[4][0],users[4][1],users[4][2],users[4][3],users[4][4])
-
-
 # test first name creation
 def test_FirstName():
     
-    
+    createAccountFunctions.storeData(users[0][0],users[0][1],users[0][2],users[0][3],users[0][4])
+    createAccountFunctions.storeData(users[1][0],users[1][1],users[1][2],users[1][3],users[1][4])
+    createAccountFunctions.storeData(users[2][0],users[2][1],users[2][2],users[2][3],users[2][4])
+    createAccountFunctions.storeData(users[3][0],users[3][1],users[3][2],users[3][3],users[3][4])
+    createAccountFunctions.storeData(users[4][0],users[4][1],users[4][2],users[4][3],users[4][4])
     passed = 0
 
     with open("firstname.txt") as file:                 
@@ -29,9 +26,19 @@ def test_FirstName():
 
     assert passed == 5
 
+    loginfunctions.clearFile("users.txt")
+    loginfunctions.clearFile("passwords.txt")
+    loginfunctions.clearFile("firstname.txt")
+    loginfunctions.clearFile("lastname.txt")
+    loginfunctions.clearFile("fullname.txt")
+
 # test last name creation
 def test_LastName():
-    
+    createAccountFunctions.storeData(users[0][0],users[0][1],users[0][2],users[0][3],users[0][4])
+    createAccountFunctions.storeData(users[1][0],users[1][1],users[1][2],users[1][3],users[1][4])
+    createAccountFunctions.storeData(users[2][0],users[2][1],users[2][2],users[2][3],users[2][4])
+    createAccountFunctions.storeData(users[3][0],users[3][1],users[3][2],users[3][3],users[3][4])
+    createAccountFunctions.storeData(users[4][0],users[4][1],users[4][2],users[4][3],users[4][4])
     
     passed = 0
 
@@ -44,9 +51,19 @@ def test_LastName():
 
     assert passed == 5
 
+    loginfunctions.clearFile("users.txt")
+    loginfunctions.clearFile("passwords.txt")
+    loginfunctions.clearFile("firstname.txt")
+    loginfunctions.clearFile("lastname.txt")
+    loginfunctions.clearFile("fullname.txt")
+
 # test full name creation
 def test_FullName():
-    
+    createAccountFunctions.storeData(users[0][0],users[0][1],users[0][2],users[0][3],users[0][4])
+    createAccountFunctions.storeData(users[1][0],users[1][1],users[1][2],users[1][3],users[1][4])
+    createAccountFunctions.storeData(users[2][0],users[2][1],users[2][2],users[2][3],users[2][4])
+    createAccountFunctions.storeData(users[3][0],users[3][1],users[3][2],users[3][3],users[3][4])
+    createAccountFunctions.storeData(users[4][0],users[4][1],users[4][2],users[4][3],users[4][4])
     
     passed = 0
 
