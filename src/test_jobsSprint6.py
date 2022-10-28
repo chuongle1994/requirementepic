244816081623c62d8b7fc56da236f29f6d8e99d7
Hyunjung Lee <leeleehj77@gmail.com>
2022-10-28 11:31:19 -0400

    Testing for the number of job posting

---
+++ src/test_jobsSprint6.py
@@ -0,0 +1,34 @@
+import createAccountFunctions, loginfunctions
+
+def clear_all_files():
+    loginfunctions.clearFile("firstname.txt")
+    loginfunctions.clearFile("lastname.txt")
+    loginfunctions.clearFile("fullname.txt")
+    loginfunctions.clearFile("users.txt")
+    loginfunctions.clearFile("passwords.txt")
+    loginfunctions.clearFile("currentUserData.txt")
+    loginfunctions.clearFile("jobPosts.json")
+
+# Testing for the number of job posting
+def test_numberOfJobposts():
+    createAccountFunctions.storeData("hyunjungl", "!Hello123", "Hyunjung", "Lee", "Hyunjung Lee")
+    loginfunctions.storeUserData("hyunjungl")
+    loginfunctions.existsJobPostsFile()
+
+    loginfunctions.createJobPost("1", "title1", "description1", "employer1", "location1", "10000", "")
+    loginfunctions.createJobPost("2", "title2", "description2", "employer2", "location2", "20000", "")
+    assert loginfunctions.getNumberOfJobPosts() == 2
+    loginfunctions.createJobPost("3", "title3", "description3", "employer3", "location3", "30000", "")
+    loginfunctions.createJobPost("4", "title4", "description4", "employer4", "location4", "40000", "")
+    assert loginfunctions.getNumberOfJobPosts() == 4
+    loginfunctions.createJobPost("5", "title5", "description5", "employer5", "location5", "50000", "")
+    loginfunctions.createJobPost("6", "title6", "description6", "employer6", "location6", "60000", "")
+    loginfunctions.createJobPost("7", "title7", "description7", "employer7", "location7", "70000", "")
+    loginfunctions.createJobPost("8", "title8", "description8", "employer8", "location8", "80000", "")
+    assert loginfunctions.getNumberOfJobPosts() == 8
+    loginfunctions.createJobPost("9", "title9", "description9", "employer9", "location9", "90000", "")
+    loginfunctions.createJobPost("10", "title10", "description10", "employer10", "location10", "100000", "")
+    loginfunctions.createJobPost("11", "title11", "description11", "employer11", "location11", "110000", "")  
+    assert loginfunctions.inputJobInfo() ==  "\nThe system can only permit up to 10 jobs to be posted."
+
+    clear_all_files()
