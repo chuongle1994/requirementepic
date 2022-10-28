ff6daa0ffcda85c97b87c129b809aea6eccc5f8a
Hyunjung Lee <leeleehj77@gmail.com>
2022-10-28 15:02:52 -0400

    Test for saving job posts

--- src/test_jobsSprint6.py
+++ src/test_jobsSprint6.py
@@ -1,4 +1,5 @@
 import createAccountFunctions, loginfunctions
+import ast
 
 def clear_all_files():
     loginfunctions.clearFile("firstname.txt")
@@ -58,5 +59,47 @@ def test_deleteJobPosts():
     # Checking for the person who has not posted a job cannot delete a job
     loginfunctions.deleteJobByIndex(0)
     assert loginfunctions.getNumberOfJobPosts() == 3
+
+    clear_all_files()
+
+def checkSaved(index, name):
+    found = 0
+    with open("savedListings.txt", "r") as file:
+        for line in file:
+            data = ast.literal_eval(line)
+            if data["Index"] == index and data["Name"] == name:
+                found = 1
+    return found
+
+# Test for saving and unsaving jobs
+def test_saveJobPost():
+    createAccountFunctions.storeData("hyunjungl", "!Hello123", "Hyunjung", "Lee", "Hyunjung Lee")
+    createAccountFunctions.storeData("trile", "Abcdef1!", "tri", "le", "tri le")
+    createAccountFunctions.storeData("dinhle", "Abcdef1!", "dinh", "le", "dinh le")
+    loginfunctions.storeUserData("hyunjungl") # current user
+    # create 4 job posts
+    loginfunctions.existsJobPostsFile()
+    loginfunctions.createJobPost("1", "title1", "description1", "employer1", "location1", "10000", "")
+    loginfunctions.createJobPost("2", "title2", "description2", "employer2", "location2", "20000", "")
+    loginfunctions.createJobPost("3", "title3", "description3", "employer3", "location3", "30000", "")
+    loginfunctions.createJobPost("4", "title4", "description4", "employer4", "location4", "40000", "")
+
+    # save
+    loginfunctions.saveJob(0, "tri le")
+    loginfunctions.saveJob(0, "dinh le")
+    loginfunctions.saveJob(1, "tri le")
+    loginfunctions.saveJob(2, "dinh le")
+
+    # check saving
+    assert checkSaved(0, "tri le") == 1
+    assert checkSaved(0, "dinh le") == 1
+    assert checkSaved(1, "tri le") == 1
+    assert checkSaved(1, "dinh le") == 0
+    assert checkSaved(2, "dinh le") == 1
+    assert checkSaved(2, "tri le") == 0
+    assert checkSaved(3, "dinh le") == 0
+
+    # unsave
+    # loginfunctions.unsaveJob()
 
     clear_all_files()
