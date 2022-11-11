37acff9abc505f3e2400a625b147b4568be89cfe
Danh Le <danhthanhle@usf.edu>
2022-11-11 13:42:17 -0500

    test for new job posted

--- src/test_notificationSprint8.py
+++ src/test_notificationSprint8.py
@@ -59,13 +59,13 @@ def test_sevenDays():
     createAccountFunctions.storeData("danhle", "Abcdef1!", "danh", "le", "danh le")
     notification.storeJobData("danh le")
 
-    #if has NOT applied in 7 days. Last date applied 11/11/2022
+    # if has NOT applied in 7 days. Last date applied 11/11/2022
     assert notification.NotApplyNotification("danh le", "11/18/2022") == 1
 
-    #update last date applied to 11/10/2022, today is 11/11/2022
+    # update last date applied to 11/10/2022, today is 11/11/2022
     notification.updateDate("danh le", "11/10/2022")
 
-    #if HAS applied in 7 days
+    # if has applied in 7 days
     assert notification.NotApplyNotification("tri le", "11/11/2022") == 0
 
     os.remove("firstname.txt")
@@ -75,28 +75,41 @@ def test_sevenDays():
     os.remove("users.txt")
     os.remove("jobNotification.txt")
 
-def test_newJobPosted():
+def test_numAppliedJobs():
+    # create accounts
     createAccountFunctions.storeData("danhle", "Abcdef1!", "danh", "le", "danh le")
     createAccountFunctions.storeData("trile", "Abcdef1!", "tri", "le", "tri le")
+
+    # create needed files
     loginfunctions.existsJobPostsFile()
     loginfunctions.existsCurrentUserData()
+
+    # logged in as danh le
     loginfunctions.storeUserData("danhle")
     notification.storeJobData("danh le")
+
+    # danh creates 2 job posts
     applicants = []
     loginfunctions.createJobPost("12345", "engineer", "code", "google", "remote", "50k", applicants)
     loginfunctions.createJobPost("54321", "engineer 2", "code 2", "google 2", "remote2", "150k", applicants)
-    #applied for no jobs
+    
+    # if applied for no jobs
     assert notification.total_appliedJob("tri le") == 0
 
+    # tri logs in and applies for first job
     loginfunctions.applyForJob(0, "tri le")
     loginfunctions.writeApp("12345", 0, "tri le", "graddate", "workdate", "description")
     os.remove("currentUserData.txt")
     loginfunctions.storeUserData("trile")
 
+    # numappliedjob should be 1
     assert notification.total_appliedJob("tri le") == 1
-    
+
+    # tri applies for second job
     loginfunctions.applyForJob(1, "tri le")
     loginfunctions.writeApp("54321", 0, "tri le", "graddate", "workdate", "description")
+
+    # numappliedjobs should be 2
     assert notification.total_appliedJob("tri le") == 2
 
     os.remove("firstname.txt")
@@ -109,5 +122,42 @@ def test_newJobPosted():
     os.remove("currentUserData.txt")
     os.remove("jobPosts.json")
 
-# def test_numAppliedJobs():
-#     return
+def test_newJobPosted():
+    # create accounts
+    createAccountFunctions.storeData("danhle", "Abcdef1!", "danh", "le", "danh le")
+    createAccountFunctions.storeData("trile", "Abcdef1!", "tri", "le", "tri le")
+
+    # create needed files
+    loginfunctions.existsJobPostsFile()
+    loginfunctions.existsCurrentUserData()
+
+    # login as danh
+    loginfunctions.storeUserData("danhle")
+
+    # create 2 job posts
+    applicants = []
+    loginfunctions.createJobPost("12345", "engineer", "code", "google", "remote", "50k", applicants)
+    loginfunctions.createJobPost("54321", "engineer 2", "code 2", "google 2", "remote2", "150k", applicants)
+
+    # login as tri
+    os.remove("currentUserData.txt")
+    loginfunctions.storeUserData("trile")
+
+    # create job data for tri
+    notification.storeJobData("tri le")
+
+    # new jobs created by danh should notify tri with num of new jobs
+    notification.saveNewJob("danh le", "engineer")
+    notification.saveNewJob("danh le", "engineer 2")
+
+    # 2 new jobs created should notify 2
+    assert notification.newJobNotification("tri le") == 2
+
+    os.remove("firstname.txt")
+    os.remove("fullname.txt")
+    os.remove("lastname.txt")
+    os.remove("passwords.txt")
+    os.remove("users.txt")
+    os.remove("jobNotification.txt")
+    os.remove("currentUserData.txt")
+    os.remove("jobPosts.json")
