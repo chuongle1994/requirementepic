7178097f6feadb8e224d53797c33ec62d1bb20bd
Hyunjung Lee <leeleehj77@gmail.com>
2022-11-20 14:32:39 -0500

    Fix errors for testing

--- src/test_API.py
+++ src/test_API.py
@@ -7,9 +7,11 @@ outputProfileFile = "MyCollege_profiles.txt"
 outputUserFile = "MyCollege_users.txt"
 outputSavedJobsFile = "MyCollege_savedJobs.txt"
 
+# Test for all output API 
 class TestAllAPIOutputFiles:
    def test_OutputJobFile(self):
-      apiFunctions.outputUsersAPI()
+      loginfunctions.existsJobPostsFile()
+      apiFunctions.outputJobApi()
       assert path.exists(outputJobFile)
    def test_OutputAppliedJobsFile(self):
       apiFunctions.outputAppliedJobsAPI()
@@ -23,14 +25,35 @@ class TestAllAPIOutputFiles:
    def test_OutputSavedJobsFile(self):
       apiFunctions.outputSavedJobsAPI()
       assert path.exists(outputSavedJobsFile)
+
 # Test for input student account API
 def test_userAccount():
-    loginfunctions.existsUserPasswordFile()
-    loginfunctions.existsFirstLastFullNameFile()
-    apiFunctions.inputAccountAPI()
-    numAccounts = createAccountFunctions.checkAccNum()
-    assert numAccounts == 5
- #Test for input jobs
+   loginfunctions.existsUserPasswordFile()
+   loginfunctions.existsFirstLastFullNameFile()
+   apiFunctions.inputAccountAPI()
+   numAccounts = createAccountFunctions.checkAccNum()
+   assert numAccounts == 5
+   clear_all_files()
+
+# Test for input jobs API
 def test_jobInput():
-    apiFunctions.inputJobsAPI()
-    assert loginfunctions.getNumberOfJobPosts() == 6
+   loginfunctions.existsJobPostsFile()
+   apiFunctions.inputJobsAPI()
+   numJobs = loginfunctions.getNumberOfJobPosts()
+   assert numJobs == 6
+   clear_all_files()
+
+# clear all created files
+def clear_all_files():
+   loginfunctions.clearFile("controls.txt")
+   loginfunctions.clearFile("firstname.txt")
+   loginfunctions.clearFile("friendList.txt")
+   loginfunctions.clearFile("fullname.txt")
+   loginfunctions.clearFile("jobNotification.txt")
+   loginfunctions.clearFile("language.txt")
+   loginfunctions.clearFile("lastname.txt")
+   loginfunctions.clearFile("membership.txt")
+   loginfunctions.clearFile("passwords.txt")
+   loginfunctions.clearFile("profile.txt")
+   loginfunctions.clearFile("users.txt")
+   loginfunctions.clearFile("jobPosts.json")
