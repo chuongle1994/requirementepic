61ac24f366d3f5419e0a5bd0d0a8f4ea13691875
Chuong <46336426+chuongle1994@users.noreply.github.com>
2022-11-20 13:55:26 -0500

    Update test_API.py
    Test input job and existing output files

--- src/test_API.py
+++ src/test_API.py
@@ -7,18 +7,22 @@ outputProfileFile = "MyCollege_profiles.txt"
 outputUserFile = "MyCollege_users.txt"
 outputSavedJobsFile = "MyCollege_savedJobs.txt"
 
-# class TestAllAPIOutputFiles:
-#     def test_OutputJobFile(self):
-#         assert path.exists(outputJobFile)
-#     def test_OutputAppliedJobsFile(self):
-#         assert path.exists(outputAppliedJobsFile)
-#     def test_OutputProfileFile(self):
-#         assert path.exists(outputProfileFile)
-#     def test_OutputUserFile(self):
-#         assert path.exists(outputUserFile)
-#     def test_OutputSavedJobsFile(self):
-#         assert path.exists(outputSavedJobsFile)
-
+class TestAllAPIOutputFiles:
+   def test_OutputJobFile(self):
+      apiFunctions.outputUsersAPI()
+      assert path.exists(outputJobFile)
+   def test_OutputAppliedJobsFile(self):
+       apiFunctions.outputAppliedJobsAPI()
+       assert path.exists(outputAppliedJobsFile)
+   def test_OutputProfileFile(self):
+       apiFunctions.outputProfileApi()
+       assert path.exists(outputProfileFile)
+   def test_OutputUserFile(self):
+       apiFunctions.outputUsersAPI()
+       assert path.exists(outputUserFile)
+   def test_OutputSavedJobsFile(self):
+          apiFunctions.outputSavedJobsAPI()
+          assert path.exists(outputSavedJobsFile)
 # Test for input student account API
 def test_userAccount():
     loginfunctions.existsUserPasswordFile()
@@ -26,6 +30,7 @@ def test_userAccount():
     apiFunctions.inputAccountAPI()
     numAccounts = createAccountFunctions.checkAccNum()
     assert numAccounts == 5
-
-
-
+ #Test for input jobs
+def test_jobInput():
+    apiFunctions.inputJobsAPI()
+    assert loginfunctions.getNumberOfJobPosts() == 6
